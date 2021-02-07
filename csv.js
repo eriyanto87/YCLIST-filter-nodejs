256f73c1d4fa1fbec87d8a454f1dfcfd93537d1c
Evisari Riyanto <evisari111012@gmail.com>
2021-02-07 11:11:44 -0800

    first commit

---
+++ csv.js
@@ -0,0 +1,32 @@
+const csv = require("csvtojson");
+const numbers = require("./numbers.json");
+const converter = require("json-2-csv");
+const fs = require("fs");
+
+csv()
+  .fromFile("./yclist_company_names.csv")
+  .then((jsonObj) => {
+    let filteredArr = [];
+    jsonObj.map((name) => filteredArr.push(name.company_name));
+    //console.log(filteredArr);
+    numbers.map((contacts) => {
+      if (
+        !filteredArr.includes(contacts["Company Name"]) &&
+        contacts["on_dnc"] !== "Y"
+      ) {
+        //     fs.writeFile("out.json", JSON.stringify(contacts), function (err) {
+        //       if (err) {
+        //         return console.log(err);
+        //       }
+        //       console.log(contacts);
+        //       // console.log("The file was saved!");
+        //     });
+        converter.json2csv(contacts, (err, csv) => {
+          if (err) {
+            throw err;
+          }
+          console.log(csv);
+        });
+      }
+    });
+  });
